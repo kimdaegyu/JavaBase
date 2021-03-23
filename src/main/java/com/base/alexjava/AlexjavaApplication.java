59773c940d29949a7b811939583fef38ed136a08
ALEX-GRAM17\Kimdaegyu <dakunara@gmail.com>
2021-03-23 13:48:43 +0900

    new project

---
+++ src/main/java/com/base/alexjava/AlexjavaApplication.java
@@ -0,0 +1,13 @@
+package com.base.alexjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/base/alexjava/AlexjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.alexjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
